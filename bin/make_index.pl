6bd63cebb1df27f33b7e95e1b009de1576ecb055
sb <sb@it-bayer.de>
2013-06-04 08:27:45 +0200

    make_index.pl umgeschrieben

--- bin/make_index.pl
+++ bin/make_index.pl
@@ -31,9 +31,11 @@ C<$start_md> geändert werden.
 my $start_md = "markdown/start.md0s";
 
 
-# index.md0x zum schreiben öffne>
+# index.md0x zum schreiben öffne
 open (INDEX, ">markdown/index.md0x");
 
+# Vortext Merker
+my $schnipp;
 
 # ----------------------------------------------------
 =head2 Überschrift der Seite
@@ -67,20 +69,20 @@ print INDEX "\n";
 # ===================================================
 =head2 Datum der start.md0s
 
-Der Inhalt der start.md sollte immer ganz oben stehen
+Der Inhalt der start.md0s sollte immer ganz oben stehen
 darum wird das Datum dieser Datei mit touch bei jedem 
 Lauf neu gesetzt.
 Somit ist sicher gestellt dass bei einer Seitenänderung
-der Inhalt der C<start.md> immer oben steht.
+der Inhalt der C<start.md0s> immer oben steht.
 
-Gesetzt wird das Datum aber nur wenn die Datei start.md vorhanden ist.
+Gesetzt wird das Datum aber nur wenn die Datei start.md0s vorhanden ist.
 
 =cut
 
 system ("/usr/bin/touch $start_md") if (-e $start_md);
 
 # ----------------------------------------------------------
-# Dateinamen der md Dateien auslesen incl. Datum der letzten Änderung
+# Dateinamen der md?? Dateien auslesen incl. Datum der letzten Änderung
 open (FH, "ls -lt1 --time-style=+\"%Y-%m-%d\" markdown/*.md* |");
 
 # ----------------------------------------------------------
@@ -113,74 +115,57 @@ vorkommen kann, auch ausgeschlossen.
 	# Verzeichnis markdown raus schneiden
 	($dummy1, my $dateiname) = split(/\//, $name);
 	#
-	# .md gegen .html austauschen
+	# .md?? gegen .html austauschen
 	(my $htmlname, $dummy1) = split(/\./, $dateiname);
 	$htmlname = $htmlname.".html";
 
 	# DIV um jeden Eintrag wickeln
-	print INDEX "<div class=\"indexliste\">";
+	print INDEX "<div class=\"indexliste\">\n";
 
 	# Datum der letzten Änderung vor dem Vortext stellen
-	print INDEX "*$datum*\n" if ($name ne $start_md);
-	# ..nicht anzeigen wenn es sich um eine start.md handelt.
+	# ... nicht setzen wenn es sich um die start.md0s handelt.
+	print INDEX "\n*$datum*\n" if ($name ne $start_md);
 
 	# Einzelnen Dateien auslesen um den Vortext zu 
 	# erstellen.
 	open (DAT, "< $name");
-	my $vortext = 0; # Vortext Merker
+
+	# Vortextmerker setzen
+	$schnipp = 0; 
 
 	while (<DAT>) {
 		chomp($_);
 
-		# Vortext Merker hoch setzen.
-		# Der Vortextmerker wird um eins erhöht
-		# wenn eine Zeile mit dem Zeichen % am Anfang
-		# gefunden wurden.
-		# Dieses Zeichen definiert die Kopfzeile von 
-		# pandoc.
-		# Werden drei solche Zeilen gefunden wird der
-		# Vortext in eine Datei geschrieben.
-		if ($_ =~ m/^\%.*/) {
-			$vortext ++;
-		}
+		# pandoc Kopfzeilen überspringen
+		# wenn schnipp noch nicht gefunden wurde.
+		next if (!$schnipp && $_ =~ m/^\%.*/ );
 
-		# Wenn
-		# <!-- schnipp -->
-		# gefunden wurde den Vortext Merker wieder zurück
-		# setzen.
-		if($_ =~ m/<!--.*schnipp.*-->/) {
-			$vortext = 0;
-		}
+		# Wenn schnipp gefunden wurde Ende des Vortextes setzen.
+		$schnipp = 1 if($_ =~ m/<!--.*schnipp.*-->/);
 
 		# Der Vortext kann auch kommentiert werden
-		# sodass er im eigentlichen Artikel nicht mehr erscheint.
-		# Diese Kommentarzeilen werden hier übersprungen.
+		# sodass er zwar auf der index.html erscheint
+		# im eigentlichen Artikel aber nicht.
 		next if ($_ =~ m/.*<!--.*/);
 		next if ($_ =~ m/.*-->.*/);
 
-
-		# Wenn am Anfang der Datei drei Zeilen mit % gefunden wurden 
-		# und die Zeile kein % mehr enthält dann den Text in
-		# die Datei schreiben.
-		if($vortext >= 3 && $_ !~ m/^\%.*/) {
-			print INDEX $_ . "\n";
-		}
+		# Vortext ausgeben wenn der Vortextmerker
+		# noch nicht gesetzt wurde und die
+		# Zeile keine pandoc Kopfzeile ist.
+		print INDEX $_ . "\n" if(!$schnipp && $_ !~ m/^\%.*/);
 	}
 
 
-	# Vortext Abschluss
-	print INDEX "[... weiter lesen]($htmlname)\n" if ($name ne
-		$start_md);
-	# .. nicht anzeigen wenn es sich um eine start.md handelt.
-	print INDEX "\n";
-	# Schlussstrich bei der start.md nicht ausgeben.
-	# ..wurde eingebaut weil in der start.md auch mal eine Fusszeile
-	#   vorkommen kann und dass dann sch... ausschaut.
-	print INDEX "-----------------------------\n" if ($name ne
-		$start_md);
-	print INDEX "\n";
+	# Vortext Abschluss und Link zur eigentlichen Seite setzen ...
+	# .. nicht anzeigen bei start.md0s.
+	print INDEX "[... weiter lesen]($htmlname)\n\n" if ($name ne $start_md);
+
+	# Schlussstrich setzen, nicht bei start.md0s.
+	print INDEX "---\n" if ($name ne $start_md);
+	
 	print INDEX "\n";
 	print INDEX "</div>\n";
+	print INDEX "\n\n\n";
 
 }
 
