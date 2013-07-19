d9eb63b698e244e8abf9e706afdd3623750f5df3
sb <sb.ccnst@amplusmail.de>
2013-07-19 11:42:35 +0200

    ....

---
+++ .bin/update_robots.txt.pl
@@ -0,0 +1,60 @@
+#!/usr/bin/perl
+
+
+=head1 update_robots.txt
+
+Liest die Seite URL aus der ../.inc/user/config.makefile
+und wechselt diese in der robots.txt aus.
+
+=cut
+
+# Aus dieser Datei wird die URL geholt
+my $make_config = "../.inc/user/config.makefile";
+
+# Diese Datei wird geändert
+my $robots = "../robots.txt";
+
+# Zwischenspeicher neue URL
+my $url;
+
+# Neuer Inhalt der Datei
+my $neu = "";
+
+# -----------------------------------------------------
+# URL der Installation holen
+open (MC, $make_config);
+
+while (<MC>) {
+	chomp $_;
+	$url = $1 if $_ =~ m/URL="(.*)"/g;
+}
+
+close(MC);
+
+
+# -----------------------------------------------------
+# Aktuellen Inhalt der robots.txt auslesen
+# und URL Tauschen
+open (RB, $robots);
+
+while (<RB>) {
+	chomp $_;
+	if ($_ =~ m/^Sitemap/) {
+		$neu .= "Sitemap: ".$url."/sitemap.xml\n";
+	} else {
+		$neu .= $_."\n";
+	}
+}
+close(RB);
+
+
+# -----------------------------------------------------
+# Neuen Inhalt zurück schreiben.
+open (RX, ">". $robots);
+
+	print RX $neu;
+
+close(RX);
+
+
+
