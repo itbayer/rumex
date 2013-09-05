3d15ac89eba15478b79fa47d30d1a4584198b6fa
sb <sb@it-bayer.de>
2013-09-05 07:49:05 +0200

    ....

--- .rumex/bin/rumex.sh
+++ .rumex/bin/rumex.sh
@@ -13,5 +13,12 @@ GVIM=/usr/bin/gvim
 # Verzeichnis wechseln
 cd $PFAD/.rx
 
+# Repository neu landen / aktualisieren
+# Diesen Schritt muss man eigentlich nicht machen
+# da ich aber an verschiedenen Rechnern Rumex bearbeite
+# und es mir schon oft passiert ist diese Befehl zu vergessen
+# wurde dieser hier eingebaut.
+git pull
+
 # (G)VIM mit rumex Erweiterung laden
-$GVIM -S ../.rumex/bin/rumex.vim -c ":let mootit_konto='$MOOTIT'"
+$GVIM -S ../.rumex/bin/rumex.gvim -c ":let mootit_konto='$MOOTIT'"
