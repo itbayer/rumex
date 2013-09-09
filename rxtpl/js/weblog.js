aeb94e949c563d4730f01ac1829ced09fe44525b
sb <sb.ccnst@amplusmail.de>
2013-09-09 11:21:10 +0200

    ....

--- rxtpl/js/weblog.js
+++ rxtpl/js/weblog.js
@@ -1,15 +1,16 @@
 /* Rumex Weblog Funktion */
 
 
-jQuery(document).ready(function() {
+$(document).ready(function() {
 
 
 	// Texte im weblog Tag ausblenden
-//	$("#seite weblog").hide();
+	$("#seite div.weblog").hide();
+	$("h1").css("cursor","pointer");
 	
 	// Überschriften raus suchen
 	$("#seite h1").click(function() {
-		$(this).next("p > weblog").css("border", "1px solid red");
+        $(this).nextAll("div.weblog").first().toggle();
 		});
 
 	});
