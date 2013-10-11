63282dc33754a64816fda0543ff2eebd5ff57198
sb <sb.ccnst@amplusmail.de>
2013-10-11 12:47:35 +0200

    ....

--- .rumex/bin/rumex-vim.sh
+++ .rumex/bin/rumex-vim.sh
@@ -5,21 +5,19 @@
 # Pfad auslesen um symbolische Links verwenden zu können
 PFAD=`readlink -f $0| perl  -ne 'chop; $_ =~ m/(.*)\/\.rumex.*/; print $1;'`
 
+# Verzeichnis wechseln
+cd $PFAD/.rx
 
 # moot.it Konto Name aus der .rx/confik.mk lesen
-#MOOTIT="rumex"
 MOOTIT=`cat config.mk | perl -ne 'chomp;print $1 if ($_ =~ m/^MOOTIT.*?=.*?"(.*)"/)'`
 
 # Autor für einen Weblog Eintrag aus der .rx/config.mk lesen
-#WEBLOGAUTOR="IT-Bayer"
 WEBLOGAUTOR=`cat config.mk | perl -ne 'chomp;print $1 if ($_ =~ m/^WEBLOGAUTOR.*?=.*?"(.*)"/)'`
 
 
 # VIM
 VIM=/usr/bin/vim
 
-# Verzeichnis wechseln
-cd $PFAD/.rx
 
 # Repository neu landen / aktualisieren
 # Diesen Schritt muss man eigentlich nicht machen
